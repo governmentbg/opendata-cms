6713ec84dbfd12a4acdfb001dfdad82e4b4c92fb
Emil Georgiev <emo@shtrak.eu>
2016-06-24 11:22:58 +0300

    default columns resize, sidebar styling

--- wp-content/themes/opendata-wp/library/widget-areas.php
+++ wp-content/themes/opendata-wp/library/widget-areas.php
@@ -13,9 +13,9 @@ function foundationpress_sidebar_widgets() {
 	  'name' => __( 'Sidebar widgets', 'foundationpress' ),
 	  'description' => __( 'Drag widgets to this sidebar container.', 'foundationpress' ),
 	  'before_widget' => '<article id="%1$s" class="widget %2$s">',
-	  'after_widget' => '</article>',
-	  'before_title' => '<h6>',
-	  'after_title' => '</h6>',
+	  'after_widget' => '</div></article>',
+	  'before_title' => '<h6 class="widget-title">',
+	  'after_title' => '</h6><div class="widget-inner">',
 	));
 
 	register_sidebar(array(
@@ -23,9 +23,19 @@ function foundationpress_sidebar_widgets() {
 	  'name' => __( 'Footer widgets', 'foundationpress' ),
 	  'description' => __( 'Drag widgets to this footer container', 'foundationpress' ),
 	  'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
-	  'after_widget' => '</article>',
-	  'before_title' => '<h6>',
-	  'after_title' => '</h6>',
+	  'after_widget' => '</div></article>',
+	  'before_title' => '<h6 class="widget-title">',
+	  'after_title' => '</h6><div class="widget-inner">',
+	));
+
+	register_sidebar(array(
+	  'id' => 'home-widgets',
+	  'name' => __( 'Home widgets', 'foundationpress' ),
+	  'description' => __( 'Widgets in this area will show up on the front page.', 'foundationpress' ),
+	  'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
+	  'after_widget' => '</div></article>',
+	  'before_title' => '<h6 class="widget-title">',
+	  'after_title' => '</h6><div class="widget-inner">',
 	));
 }
 
