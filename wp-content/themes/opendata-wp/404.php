778d45623c42d615825c3390202e03ed47dead67
Emil Georgiev <emo@shtrak.eu>
2016-06-28 23:30:58 +0300

    fix 404 page general layout

--- wp-content/themes/opendata-wp/404.php
+++ wp-content/themes/opendata-wp/404.php
@@ -9,7 +9,7 @@
 get_header(); ?>
 
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
+	<div class="small-12 medium-9 columns medium-push-3" role="main">
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
