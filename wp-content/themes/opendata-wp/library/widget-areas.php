21edd0d7388282479e2da742416656a1a42e3f8f
Emil Georgiev <emo@shtrak.eu>
2016-06-28 12:29:17 +0300

    Fix for title-less widgets breaking the DOM

--- wp-content/themes/opendata-wp/library/widget-areas.php
+++ wp-content/themes/opendata-wp/library/widget-areas.php
@@ -12,30 +12,30 @@ function foundationpress_sidebar_widgets() {
 	  'id' => 'sidebar-widgets',
 	  'name' => __( 'Sidebar widgets', 'foundationpress' ),
 	  'description' => __( 'Drag widgets to this sidebar container.', 'foundationpress' ),
-	  'before_widget' => '<article id="%1$s" class="widget %2$s">',
-	  'after_widget' => '</div></article>',
-	  'before_title' => '<h6 class="widget-title">',
-	  'after_title' => '</h6><div class="widget-inner">',
+	  'before_widget' => '<article id="%1$s" class="widget %2$s"><div class="widget-inner notitle">', //Opening widget-inner, so we don't break the DOM if there is no title.
+	  'after_widget' => '</div></article>', //Close widget-inner and then the whole thing.
+	  'before_title' => '</div><h6 class="widget-title">', //If there is a title, close widget-inner so we can open it again, leaving the title out.
+	  'after_title' => '</h6><div class="widget-inner">', //Open widget-inner after the title.
 	));
 
 	register_sidebar(array(
 	  'id' => 'footer-widgets',
 	  'name' => __( 'Footer widgets', 'foundationpress' ),
 	  'description' => __( 'Drag widgets to this footer container', 'foundationpress' ),
-	  'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
-	  'after_widget' => '</div></article>',
-	  'before_title' => '<h6 class="widget-title">',
-	  'after_title' => '</h6><div class="widget-inner">',
+	  'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s"><div class="widget-inner notitle">', //Opening widget-inner, so we don't break the DOM if there is no title.
+	  'after_widget' => '</div></article>', //Close widget-inner and then the whole thing.
+	  'before_title' => '</div><h6 class="widget-title">', //If there is a title, close widget-inner so we can open it again, leaving the title out.
+	  'after_title' => '</h6><div class="widget-inner">', //Open widget-inner after the title.
 	));
 
 	register_sidebar(array(
 	  'id' => 'home-widgets',
 	  'name' => __( 'Home widgets', 'foundationpress' ),
 	  'description' => __( 'Widgets in this area will show up on the front page.', 'foundationpress' ),
-	  'before_widget' => '<article id="%1$s" class="widget %2$s">',
-	  'after_widget' => '</div></article>',
-	  'before_title' => '<h6 class="widget-title">',
-	  'after_title' => '</h6><div class="widget-inner">',
+	  'before_widget' => '<article id="%1$s" class="widget %2$s"><div class="widget-inner notitle">', //Opening widget-inner, so we don't break the DOM if there is no title.
+	  'after_widget' => '</div></article>', //Close widget-inner and then the whole thing.
+	  'before_title' => '</div><h6 class="widget-title">', //If there is a title, close widget-inner so we can open it again, leaving the title out.
+	  'after_title' => '</h6><div class="widget-inner">', //Open widget-inner after the title.
 	));
 }
 
