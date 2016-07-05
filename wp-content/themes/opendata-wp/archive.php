cd5fc8bd9da4f2691eb3a380a87a22994e524bb7
Emil Georgiev <emo@shtrak.eu>
2016-07-05 13:53:37 +0300

    fix archive pages layout issue, use excerpt on archives

--- wp-content/themes/opendata-wp/archive.php
+++ wp-content/themes/opendata-wp/archive.php
@@ -23,11 +23,11 @@ get_header(); ?>
 
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+			<?php get_template_part( 'template-parts/excerpt', get_post_format() ); ?>
 		<?php endwhile; ?>
 
 		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part( 'template-parts/excerpt', 'none' ); ?>
 
 		<?php endif; // End have_posts() check. ?>
 
---
+++ wp-content/themes/opendata-wp/template-parts/excerpt.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The default template for displaying excerpt
+ * it's the same as the content.php part excerpt it uses the excerpt
+ * instead of the content.
+ *
+ * Used for both archive and search
+ */
+
+?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
+	<header>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php foundationpress_entry_meta(); ?>
+	</header>
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div>
+	<footer>
+		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
+	</footer>
+	<hr />
+</div>
