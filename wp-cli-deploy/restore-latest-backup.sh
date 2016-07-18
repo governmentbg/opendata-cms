a768680e8f63079c6115c84d578326f1cda91a42
Vladimir Vassilev <vlood.vassilev@gmail.com>
2016-07-18 16:15:49 +0300

    more config files and a rename

--- wp-cli-deploy/restore-latest-backup.sh
+++
@@ -1,9 +0,0 @@
-#backup current db
-
-echo '--------';
-echo 'Let us have a backup of the database before restoring (WP-CLI export)';
-echo '--------';
-
-wp db export ~/dbbackup-before-restore.sql;
-
-wp db import ~/latest-db-dump.sql;
