173ec3a258bd7281692e75011aa20777664d8409
Emil Georgiev <emo@shtrak.eu>
2016-07-19 21:19:12 +0300

    Backup script filenames and structure enhancements Now creates a directory in ~, containing latest backups + saving older ones.

--- backup-restore/backup.sh
+++ backup-restore/backup.sh
@@ -1,18 +1,27 @@
 last_backup_time=$(date "+%Y-%m-%d-%H-%M-%S");
-backup_file_name="db-backup_$last_backup_time.sql";
+backup_file_name="$last_backup_time-DB.sql";
+backup_dir=~/opendata-cms-backup
 
 echo '--------';
 echo 'Starting Database backup (WP-CLI export)';
 echo '--------';
 
-wp db export ~/$backup_file_name;
-wp db export ~/latest-db-dump.sql;
+if [ ! -d "$backup_dir" ]; then
+  mkdir $backup_dir;
+  mkdir $backup_dir/older;
+fi
+
+mkdir $backup_dir/older/$last_backup_time
+wp db export ~/opendata-cms-backup/latest-db-dump.sql;
+wp db export ~/opendata-cms-backup/older/$last_backup_time/$backup_file_name;
 
 echo '--------';
 echo 'Starting file backup';
 echo '--------';
 
-tar -vczf ~/files-backup_$last_backup_time.gz .;
+tar -vczf ~/opendata-cms-backup/files-backup_latest.gz .;
+cp ~/opendata-cms-backup/files-backup_latest.gz ~/opendata-cms-backup/older/$last_backup_time/$last_backup_time-files.gz
 
 echo '--------';
-echo 'Backup complete';
+echo 'Backup complete.';
+echo 'You can find the archived files in ' $backup_dir;
