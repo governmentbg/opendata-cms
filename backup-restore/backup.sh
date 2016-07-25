f8306fa136aaca5c7c5ccd483c8be90d0bbce591
Emil Georgiev <emo@shtrak.eu>
2016-07-25 15:22:19 +0300

    backup/restore scripts can now be run from anywhere

--- backup-restore/backup.sh
+++ backup-restore/backup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-config="./backup-restore.config";
+
+script_root_dir=`dirname "$0"`
+config="$script_root_dir/backup-restore.config"
+site_path="$1"
+
+if [ -z "$site_path" ]
+then
+  echo "Usage: $0 /path/to/your/site"
+  exit 1
+fi
+
 last_backup_time=$(date "+%Y-%m-%d-%H-%M-%S");
 backup_file_name="$last_backup_time-DB.sql";
 
@@ -10,7 +20,7 @@ else
   echo "--------------"
   echo "Sorry, I couldn't find a 'backup-restore.config' file. Copy backup-restore.config.example to backup-restore.config and fill in all the required information inside to continue.";
   echo "--------------"
-  exit 0;
+  exit 2;
 fi
 
 echo '--------';
@@ -24,7 +34,7 @@ fi
 
 mkdir $backup_dir/older/$last_backup_time # Create a Directory with the current timestamp so older backups are easier to find.
 
-wp db export $backup_dir/latest-db-dump.sql; # Export the database as 'latest'. it the export is successfull, copy the sql file to its respective timestamp directory.
+wp --path=$site_path db export $backup_dir/latest-db-dump.sql; # Export the database as 'latest'. If the export is successfull, copy the sql file to its respective timestamp directory.
 if [ $? -eq 0 ]; then
     echo "***** Done! The database was exported successfully.";
     db_export_status="OK";
@@ -38,7 +48,7 @@ echo '--------';
 echo 'Starting file backup';
 echo '--------';
 
-tar -vczf $backup_dir/latest-files-backup.tar.gz ./../..; # Archive all site files to a tar.gz and if successfull, copy the archive to its respective timestamp directory.
+tar -vczf $backup_dir/latest-files-backup.tar.gz $site_path; # Archive all site files to a tar.gz and if successfull, copy the archive to its respective timestamp directory.
 if [ $? -eq 0 ]; then
     cp $backup_dir/latest-files-backup.tar.gz $backup_dir/older/$last_backup_time/$last_backup_time-files.tar.gz;
     echo "***** Done! All site files were archived successfully.";
--- backup-restore/restore.sh
+++ backup-restore/restore.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-config="./backup-restore.config";
+script_root_dir=`dirname "$0"`
+config="$script_root_dir/backup-restore.config"
+site_path="$1"
+
+if [ -z "$site_path" ]
+then
+  echo "Usage: $0 /path/to/your/site"
+  exit 1
+fi
 
 if [ -f "$config" ]
 then
-  . $config; #if restore.config is present, use it as source
+  . $config #if restore.config is present, use it as source
 else
-  echo "Sorry, I couldn't find a 'backup-restore.config' file. Copy backup-restore.config.example to backup-restore.config and fill in all the required information inside to continue.";
-  exit 0;
+  echo "Sorry, I couldn't find a 'backup-restore.config' file. Copy backup-restore.config.example to backup-restore.config and fill in all the required information inside to continue."
+  exit 0
 fi
 
-echo '--------';
-echo 'Enabling maintenance mode';
-echo '--------';
+echo '--------'
+echo 'Enabling maintenance mode'
+echo '--------'
 
 #You can change the message the site outputs during maintenance mode in the config file.
-echo $maintenance_msg > ../../.maintenance;
+echo $maintenance_msg > $site_path/.maintenance;
 if [ $? -eq 0 ]; then
     echo "***** Done! Maintenance mode enabled.";
     maintenance_mode_enable_status="OK";
@@ -28,13 +36,13 @@ echo '--------';
 echo 'Backup the current database, just in case. A backup of the backup if you will. You can find it in ' $backup_dir;
 echo '--------';
 
-wp db export $backup_dir/dbbackup-before-restore.sql;
+wp --path=$site_path db export $backup_dir/dbbackup-before-restore.sql;
 
 echo '--------';
 echo 'Restoring the database...';
 echo '--------';
 
-wp db import $backup_dir/latest-db-dump.sql;
+wp --path=$site_path db import $backup_dir/latest-db-dump.sql;
 if [ $? -eq 0 ]; then
     echo "***** Done! The database was successfully imported.";
     db_import_status="OK";
@@ -47,7 +55,7 @@ echo '--------';
 echo 'Now extracting the files from the backup archive';
 echo '--------';
 
-tar -zxvf $backup_dir/latest-files-backup.tar.gz -C ./../../;
+tar -zxvf $backup_dir/latest-files-backup.tar.gz -C $site_path/;
 if [ $? -eq 0 ]; then
     echo "***** Done! The site files were successfully extracted from the backup archive.";
     files_extract_status="OK";
@@ -59,7 +67,7 @@ fi
 echo '--------';
 echo 'Disabling maintenance mode';
 echo '--------';
-rm ../../.maintenance;
+rm $site_path/.maintenance;
 if [ $? -eq 0 ]; then
     echo "***** Done! Maintenance mode disabled.";
     maintenance_mode_disable_status="OK";
